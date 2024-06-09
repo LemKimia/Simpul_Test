27a82eb920da2363ecf0a6582d03c1a8ebd27b08
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-09 23:30:16 +0700

    feat: added context provider to the parent component

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
-import App from '@/routes'
-import '@/styles/index.css'
-import '@/styles/bubble.css'
+import App from "@/routes";
+import "@/styles/index.css";
+import "@/styles/bubble.css";
 
-import { InboxProvider } from './utils/context/inbox-context'
+import { InboxProvider } from "@/utils/context/inbox-context";
+import { InteractionProvider } from "@/utils/context/interaction-context";
 
-import { Toaster } from 'sonner'
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <InboxProvider>
-    <App />
-    <Toaster />
+    <InteractionProvider>
+      <App />
+      <Toaster />
+    </InteractionProvider>
   </InboxProvider>
 );

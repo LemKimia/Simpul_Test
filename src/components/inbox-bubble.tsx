bf108458628f3ea4e8803a4c82a949c91473508c
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-09 23:31:27 +0700

    feat: work on progress on hompeage and inbox bubble

--- src/components/inbox-bubble.tsx
+++ src/components/inbox-bubble.tsx
@@ -1,41 +1,49 @@
-import { getComment } from "@/utils/api/api";
-import { useEffect, useState } from "react";
-import { toast } from "sonner";
+import { useContext } from "react";
 
-const ChatBubble = () => {
-  const [chatList, setChatList] = useState<Comment>();
-  const [mode, setMode] = useState<"list" | "detail">("list");
+import { InboxContext } from "@/utils/context/inbox-context";
+import { useInteraction } from "@/utils/context/interaction-context";
 
-  const fetchData = async () => {
-    try {
-      const response = await getComment();
+const InboxBubble = () => {
+  const { roomList } = useContext(InboxContext);
+  const { actions } = useInteraction();
 
-      setChatList(response.data);
-    } catch (error) {
-      toast((error as Error).message.toString());
-    }
+  const handleCloseInboxClick = () => {
+    actions.setVisible("InboxBubble", false);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
-    <div className="bubble flex flex-col">
+    <div className="bubble flex flex-col gap-5">
       <div>
-        <img src="public\Search Bar.png" alt="Search Bar" />
+        <img
+          src="public\Search Bar.png"
+          alt="Search Bar"
+          onClick={handleCloseInboxClick}
+        />
       </div>
-      {mode === "list" ? (
-        <div>
-          <p>list</p>
-        </div>
-      ) : (
-        <div>
-          <p>detail</p>
-        </div>
-      )}
+      <div className="">
+        <ul>
+          {roomList.map((room) => (
+            <li key={room.id} className="grid grid-cols-6 gap-5 my-10">
+              <div className="flex flex-row ">
+                <img src="public\Group 15.png" alt="" className="w-10 h-10" />
+                <img src="public\Group 687.png" alt="" className="w-10 h-10" />
+              </div>
+              <div className="flex flex-col gap-2 col-span-5">
+                <div className="flex flex-row gap-5">
+                  <p className="text-sm">{room.title}</p>
+                  <p className="text-xs">{room.createdAt}</p>
+                </div>
+                <div className="text-sm">
+                  <p className="font-bold">{room.lastMessage.user.name}</p>
+                  <p>{room.lastMessage.user.message}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default ChatBubble;
+export default InboxBubble;
--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,12 +1,28 @@
-import ChatBubble from "@/components/inbox-bubble";
+import ChatRoom from "@/components/chat-room";
+import InboxBubble from "@/components/inbox-bubble";
 import Navbar from "@/components/navbar";
 import TaskBubble from "@/components/task-bubble";
-import { useState } from "react";
+
+import { useInteraction } from "@/utils/context/interaction-context";
 
 const Homepage = () => {
-  const [onClick, setOnClick] = useState(false);
-  const [showTask, setShowTask] = useState(false);
-  const [showInbox, setShowInbox] = useState(false);
+
+  const { states, actions } = useInteraction();
+
+  const handleOpenInboxClick = () => {
+    actions.setClicked("InboxBubble", true);
+  };
+
+  const handleTaskClick = () => {
+    actions.setClicked("TaskBubble", true);
+  };
+
+
+  const isChatVisible = states["InboxBubble"]?.visible || false;
+  const isTaskVisible = states["TaskBubble"]?.visible || false;
+  const chatMode = states["chatMode"]?.mode || "list"
+
+  console.log("Current mode state:", states["InboxBubble"]?.mode);
 
   return (
     <div className="grid grid-cols-6 bg-[#4F4F4F] h-screen ">
@@ -17,24 +33,26 @@ const Homepage = () => {
         </div>
         <div className="grid grid-flow-col-dense justify-items-end content-center">
           <img
-            src="public\Trigger 1.png"
+            src="/Trigger 1.png"
             alt="trigger button"
             className="order-2"
-            onMouseEnter={() => setOnClick(true)}
-            onClick={() => setOnClick(false)}
+            onMouseEnter={() => actions.setClicked("Trigger", true)}
+            onClick={() => actions.setClicked("Trigger", false)}
           />
-          {onClick && (
+          {states["Trigger"]?.clicked && (
             <div className="flex flex-row col-span-6 items-stretch order-1">
               <div className="flex flex-col">
                 <p>Task</p>
                 <img
-                  src="public\Task.png"
+                  src="/Task.png"
                   alt="task image"
                   className="w-20"
-                  onClick={() => setShowTask(true)}
+                  onClick={handleTaskClick}
+                  // onMouseEnter={() => handleMouseEnter("TaskBubble")}
+                  // onMouseLeave={() => handleMouseLeave("TaskBubble")}
                 />
-                {showTask && (
-                  <div onClick={() => setShowTask(false)}>
+                {isTaskVisible && (
+                  <div onClick={() => actions.setVisible("TaskBubble", false)}>
                     <TaskBubble />
                   </div>
                 )}
@@ -42,14 +60,23 @@ const Homepage = () => {
               <div className="flex flex-col">
                 <p>Chat</p>
                 <img
-                  src="public\Chat.png"
+                  src="/Chat.png"
                   alt="chat image"
                   className="w-20"
-                  onClick={() => setShowInbox(true)}
+                  onClick={handleOpenInboxClick}
                 />
-                {showInbox && (
-                  <div onClick={() => setShowInbox(false)}>
-                    <ChatBubble />
+                {isChatVisible && (
+                  <div>
+                    {chatMode === "list" ? (
+                      <div>
+                        <InboxBubble />
+                      </div>
+                    ) : (
+                      <div>
+                        <ChatRoom />
+                      </div>
+                    )}
+                    
                   </div>
                 )}
               </div>

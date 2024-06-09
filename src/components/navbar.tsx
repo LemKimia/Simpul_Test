fa8eec4c070c2ac6a35a4c54dc47810e26ee7692
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-10 01:02:57 +0700

    feat: updating navbar on homepage

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,11 @@
-
+import { Input } from "@/components/ui/input";
 
 const Navbar = () => {
   return (
-    <nav className="w-full bg-[#828282]">
-      <div>
-        <img src="/Search Icon.png" alt="pokedex-logo" />
+    <nav className="w-full h-max bg-[#4F4F4F]">
+      <div className="flex flex-row p-2">
+        <img src="/Search Icon.png" alt="pokedex-logo" className="size-4" />
+        <Input className="bg-[#4F4F4F] border-none ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 h-5 text-white" />
       </div>
     </nav>
   );

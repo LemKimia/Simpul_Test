9873d798f04298bad4eeeb6b88ea807e7f519bae
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2024-06-10 11:52:34 +0700

    feat: added route loader

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,25 +3,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "@/pages/homepage";
 import Cover from "@/pages";
 
-
-
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Cover />,
+    loader: () => {
+      return "Simple Quicks";
+    },
   },
   {
     path: "/home",
     element: <Homepage />,
+    loader: () => {
+      return "Simple Quicks - Home";
+    },
   },
 ]);
 
 const app = () => {
-  return (
-   
-      <RouterProvider router={router} />
-  
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default app;
